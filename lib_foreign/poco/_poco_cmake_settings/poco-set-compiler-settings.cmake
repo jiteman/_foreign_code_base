b3f69052bce250a5f3f0b5a4597a60e620a9ddbe
Jiteman <jiteman@gmail.com>
2018-12-26 19:52:06 +0300

    scripts has been updated for functions-and-macroses using

--- lib_foreign/poco/_poco_cmake_settings/poco-set-compiler-settings.cmake
+++ lib_foreign/poco/_poco_cmake_settings/poco-set-compiler-settings.cmake
@@ -4,16 +4,10 @@ JMSD_CMAKE_CURRENT_FILE_IN( "${JMSD_FOREIGN_COMPONENT_NAME}-set-compiler-setting
 
 
 if ( UNIX )
-	if ( JMSD_CMAKE_VERBOSE_OUTPUT_IS_ON )
-		message( STATUS "[JMSD] ${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Linux linker settings are selected" )
-	endif()
-	
+	JMSD_SHOW_MESSAGE( "${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Linux linker settings are selected" )
 	include( ${JMSD_POCO_CMAKE_SETTINGS_PATH}/compiler-settings/${JMSD_FOREIGN_COMPONENT_NAME}-set-linux-compiler-settings.cmake )
 elseif( WIN32 )
-	if ( JMSD_CMAKE_VERBOSE_OUTPUT_IS_ON )
-		message( STATUS "[JMSD] ${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Windows linker settings are selected" )
-	endif()
-	
+	JMSD_SHOW_MESSAGE( "${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Windows linker settings are selected" )
 	include( ${JMSD_POCO_CMAKE_SETTINGS_PATH}/compiler-settings/${JMSD_FOREIGN_COMPONENT_NAME}-set-windows-compiler-settings.cmake )
 else()
 	message( STATUS "[JMSD] ${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Unsupported platform. Default settings are used." )
