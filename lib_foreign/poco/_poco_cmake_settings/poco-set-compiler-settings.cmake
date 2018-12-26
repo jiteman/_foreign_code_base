b36c36634e5be406557f0d7dc9b6f4beac78abc3
jiteman <jiteman@gmail.com>
2018-12-26 06:23:34 +0000

    poco compiler setting separating has been started...

--- lib_foreign/poco/_poco_cmake_settings/poco-set-compiler-settings.cmake
+++ lib_foreign/poco/_poco_cmake_settings/poco-set-compiler-settings.cmake
@@ -1,30 +1,23 @@
-include( ${JMSD_CMAKE_SETTINGS_PATH}/set-verbosity-configuration.cmake )
+set( JMSD_FOREIGN_COMPONENT_NAME poco )
 
-JMSD_CMAKE_CURRENT_FILE_IN( "set-compiler-settings.cmake" )
+JMSD_CMAKE_CURRENT_FILE_IN( "${JMSD_FOREIGN_COMPONENT_NAME}-set-compiler-settings.cmake" )
+
 
 if ( UNIX )
 	if ( JMSD_CMAKE_VERBOSE_OUTPUT_IS_ON )
-		message( STATUS "[JMSD] COMPILER SETTINGS: Linux linker settings are selected" )
+		message( STATUS "[JMSD] ${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Linux linker settings are selected" )
 	endif()
 	
-	include( ${JMSD_CMAKE_SETTINGS_PATH}/compiler-settings/set-linux-compiler-settings.cmake )
-
+	include( ${JMSD_POCO_CMAKE_SETTINGS_PATH}/compiler-settings/${JMSD_FOREIGN_COMPONENT_NAME}-set-linux-compiler-settings.cmake )
 elseif( WIN32 )
 	if ( JMSD_CMAKE_VERBOSE_OUTPUT_IS_ON )
-		message( STATUS "[JMSD] COMPILER SETTINGS: Windows linker settings are selected" )
+		message( STATUS "[JMSD] ${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Windows linker settings are selected" )
 	endif()
 	
-	include( ${JMSD_CMAKE_SETTINGS_PATH}/compiler-settings/set-windows-compiler-settings.cmake )
-
+	include( ${JMSD_POCO_CMAKE_SETTINGS_PATH}/compiler-settings/${JMSD_FOREIGN_COMPONENT_NAME}-set-windows-compiler-settings.cmake )
 else()
-	message( STATUS "[JMSD] COMPILER SETTINGS: Unsupported platform. Default settings are used." )
-
+	message( STATUS "[JMSD] ${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: Unsupported platform. Default settings are used." )
 endif()
 
-
-#CONFIG += c++1z
-#CONFIG += exceptions
-#CONFIG += depend_includepath
-#CONFIG += incremental
 
 JMSD_CMAKE_CURRENT_FILE_OUT( "set-compiler-settings.cmake" )
