b5facc71951126d3b6c0a9b584352bb83d62c384
jiteman <jiteman@gmail.com>
2019-01-03 20:13:17 +0300

    seven zip compiller setting separation has been done

--- lib_foreign/poco/_poco_cmake_settings/compiler-settings/poco-seven-zip-set-linux-compiler-settings.cmake
+++ lib_foreign/poco/_poco_cmake_settings/compiler-settings/poco-seven-zip-set-linux-compiler-settings.cmake
@@ -3,15 +3,11 @@ JMSD_CMAKE_CURRENT_FILE_IN( "${JMSD_FOREIGN_COMPONENT_NAME}-set-linux-compiler-s
 if ( UNIX )
 	set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
 
-	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "-Wno-" ) #
-
 	string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
 
-	## string( REPLACE "-W" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) #
+	string( REPLACE "-Wredundant-decls" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) # redundant redeclaration of '' in same scope
 
 	set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
-
-	## set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-format" )
 else()
 	message( SEND_ERROR "[JMSD] ${JMSD_FOREIGN_COMPONENT_NAME} COMPILER SETTINGS: ${JMSD_FOREIGN_COMPONENT_NAME}-set-linux-compiler-settings.cmake is included while not on linux" )
 
