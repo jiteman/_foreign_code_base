dbf92f35fa1c0c3d350c4df3f006cc72e5678892
jiteman <jiteman@gmail.com>
2019-01-04 17:37:38 +0300

    foreign library example addition has been started

---
+++ lib_foreign/foreign_library_example/_fle_cmake_settings/compiler-settings/fle-common-set-linux-compiler-settings.cmake
@@ -0,0 +1,20 @@
+JMSD_CMAKE_CURRENT_FILE_IN( "poco-common-set-linux-compiler-settings.cmake" )
+
+if ( UNIX )
+	## set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+
+	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "-Wno-" ) #
+
+	##string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
+
+	## string( REPLACE "-W" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) #
+
+	## set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
+
+	## set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-format" )
+else()
+	message( SEND_ERROR "[JMSD] poco-common COMPILER SETTINGS: poco-common-set-linux-compiler-settings.cmake is included while not on linux" )
+
+endif()
+
+JMSD_CMAKE_CURRENT_FILE_OUT( "poco-common-set-linux-compiler-settings.cmake" )
---
+++ lib_foreign/foreign_library_example/_fle_cmake_settings/compiler-settings/fle-common-set-windows-compiler-settings.cmake
@@ -0,0 +1,18 @@
+JMSD_CMAKE_CURRENT_FILE_IN( "poco-common-set-windows-compiler-settings.cmake" )
+
+if ( WIN32 )
+	## set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+
+	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "/wd" ) #
+
+	## string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
+
+	## string( REPLACE "X" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) #
+
+	## set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
+else()
+	message( SEND_ERROR "[JMSD] poco-common COMPILER SETTINGS: poco-common-set-windows-compiler-settings.cmake is included while not on windows" )
+
+endif()
+
+JMSD_CMAKE_CURRENT_FILE_OUT( "poco-common-set-windows-compiler-settings.cmake" )
